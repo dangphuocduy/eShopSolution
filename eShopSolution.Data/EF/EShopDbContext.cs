c9a2115d54f4e97c229a148af7fe3c351d51ee13
duydp <duydp@softech.vn>
2020-08-10 23:11:25 +0700

    Add Entity

---
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,20 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {           
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
